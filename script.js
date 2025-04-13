233a133237257c0f601126bebc91f1089367e495
aadya909 <145979679+aadya909@users.noreply.github.com>
2025-04-14 00:46:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+const buttons = document.querySelectorAll('.btn');
+const stopButton = document.querySelector('.stop');
+
+let currentAudio = null;
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const sound = button.getAttribute('data-sound');
+    const audio = new Audio(`sounds/${sound}.mp3`);
+
+    if (currentAudio) {
+      currentAudio.pause();
+      currentAudio.currentTime = 0;
+    }
+
+    currentAudio = audio;
+    currentAudio.play();
+  });
+});
+
+stopButton.addEventListener('click', () => {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+    currentAudio = null;
+  }
+});
+
